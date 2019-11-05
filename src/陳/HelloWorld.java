230f850d6fc3ec82dd02beaec9c9a8f943dc4127
sdnccu <57346523+sdnccu@users.noreply.github.com>
2019-11-05 10:26:52 +0800

    P17

---
+++ src/陳/HelloWorld.java
@@ -0,0 +1,58 @@
+package 陳;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.layout.FillLayout;
+
+public class HelloWorld {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			HelloWorld window = new HelloWorld();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		Composite composite = new Composite(shell, SWT.NONE);
+		composite.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		Button btnNewButton = new Button(composite, SWT.NONE);
+		btnNewButton.setText("Welcome, 冠盛！");
+
+	}
+}
